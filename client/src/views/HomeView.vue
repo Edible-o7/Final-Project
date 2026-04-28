8c84eeb81d62a58c36233bbf167ae0f1885ea976
Edible <pomajre1@newpaltz.edu>
2026-04-27 23:52:30 -0400

    Fixed minor spelling mistake

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -5,7 +5,7 @@
   <main>
     <section class="section">
       <div class="container">
-        <h1 class="title">Welcome to my fitness tracing app.</h1>
+        <h1 class="title">Welcome to my fitness tracking app.</h1>
         <p class="subtitle">Please log in to see your personal activity feed and stats.</p>
       </div>
     </section>
